04b9362fd08b6a143cbb5162428e9e465a625b53
Giovanna Dias <giovannaofdias@gmail.com>
2026-03-31 12:51:29 -0300

    feat: Adicionando banner para página Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,23 @@
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
-    <section>
-        <h2>Home</h2>
-    </section>
+    <div className='home'>
+      {/* banner */}
+      <section className='hero'>
+        <div className='hero-container'>
+          <div className='badge'>Academia Inteligente</div>
+          <h1>TRANSFORME<br/> SEU <span>CORPO</span><br/> COM INTELIGÊNCIA</h1>
+          <p className='subtitulo'>Treinos personalizados por IA, acompanhamento em tempo real e a estrutura mais moderna de São Paulo</p>
+          <div className='hero-btn'>
+            <Link to="/planos" className='btn-planos'>Conheça nossos planos</Link>
+            <Link to="/contato" className='btn-outline'>Calcular IMC</Link>
+          </div>
+        </div>
+        <div className='hero-scroll'>↓ scroll</div>
+      </section>
+      
+    </div>
   )
 }
 
