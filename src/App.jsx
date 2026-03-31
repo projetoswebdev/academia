4bf1f7dbc502d3ebd8b3d6d82b844e7271dcb07c
maria <mariadruzeic.06@gmail.com>
2026-03-30 22:37:44 -0300

    feat: configurando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Error from './routes/Error'
+import Home from './routes/Home'
+import Imc from './routes/Imc'
+import Sobre from './routes/Sobre'
+import Contato from './routes/Contato'
+import Planos from './routes/Planos'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
 
 function App() {
 
-  return (
-    <>
 
-    </>
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contato" element={<Contato/>}/>
+        <Route path="/sobre" element={<Sobre/>}/>
+        <Route path="/imc" element={<Imc/>}/>
+        <Route path="/planos" element={<Planos/>}/>
+
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
