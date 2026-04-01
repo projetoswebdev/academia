7f153c339fa0ad9d1329703cfde38d9e6fdf9016
Giovanna Dias <giovannaofdias@gmail.com>
2026-04-01 15:42:25 -0300

    feat: criando cabeçalho da página de planos

--- src/routes/Planos.jsx
+++ src/routes/Planos.jsx
@@ -1,9 +1,15 @@
 
 const Planos = () => {
   return (
-    <section>
-        <h2>Planos</h2>
-    </section>
+    <div className="pagina-plano">
+      <div className="container-plano">
+        <p className="plano-cabecalho">Nossos Planos</p>
+        <h1 className="info-titulo">INVISTA NO SEU<br/><span>MELHOR EU</span></h1>
+        <p className="planos-subtitulo">
+          Escolha o plano que se encaixa no seu ritmo. Todos incluem acesso ao app ForceX e avaliação física.
+        </p>
+      </div>
+    </div>
   )
 }
 
