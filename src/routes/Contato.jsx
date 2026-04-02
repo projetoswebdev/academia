c05d4f5a2d9ffee133ba164744f8b678eeddfc5e
Marianne <mariannemukai@gmail.com>
2026-04-02 16:16:09 -0300

    feat: criando estrutura do formulário e adicionando campos de contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,6 +1,24 @@
 const Contato = () => {
   return (
     <section>
-      {/* Título da página */}
+     
       <h2>Contato</h2>
       <p>Entre em contato com a nossa Academia Inteligente.</p>
+
+       
+      <form>
+        {/* Seção nome */}
+        <label htmlFor="nome">Nome</label>
+        <input type="text" id="nome" name="nome" />
+
+        {/* Seção email */}
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        {/* Seção telefone */}
+        <label htmlFor="telefone">Telefone</label>
+        <input type="tel" id="telefone" name="telefone" />
+
+        {/* Seção mensagem */}
+        <label htmlFor="mensagem">Mensagem</label>
+        <textarea id="mensagem" name="mensagem"></textarea>
